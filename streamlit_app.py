b67dc9ed1ab241778fd0f366b8563828d7cc33a4
rjenki08 <rachel.arrington@advocatehealth.org>
2026-04-28 14:58:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input("Name of Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select((col('FRUIT_NAME')))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -30,15 +30,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
     
            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
-    
+ 
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
@@ -46,6 +43,3 @@ if time_to_insert:
 
         st.success(f"Your Smoothie is ordered, Order name: {name_on_order}", icon='✅')
   
-
-
-
