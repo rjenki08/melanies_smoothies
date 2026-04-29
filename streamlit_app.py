f203b4cf5cac690fe334182a4b776ad5b7bd387e
rjenki08 <rachel.arrington@advocatehealth.org>
2026-04-29 10:48:32 -0400

    Update streamlit_app.py
    added search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write(
 name_on_order = st.text_input("Name of Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAME')
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+ST.STOP()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
