e90b540914af18c63a08ca8488e1b0f202704da2
rjenki08 <rachel.arrington@advocatehealth.org>
2026-04-29 10:16:09 -0400

    Update streamlit_app.py
    removed duplicate response url

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if time_to_insert:
         st.success(f"Your Smoothie is ordered, Order name: {name_on_order}" , icon='✅')
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
     
