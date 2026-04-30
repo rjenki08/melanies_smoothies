a7449b5513897976ba16a718c04d7d43d5a2042f
rjenki08 <rachel.arrington@advocatehealth.org>
2026-04-30 11:58:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 import requests 
 import pandas as pd
 
-pd_df=my_dataframe.to_pandas()
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -20,7 +20,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-
+pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
