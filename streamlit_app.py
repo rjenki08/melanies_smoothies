da3b1537e2d41e29c1f334d8411f257cf12fa6d0
rjenki08 <rachel.arrington@advocatehealth.org>
2026-04-30 11:31:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,6 @@ st.write(
 name_on_order = st.text_input("Name of Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
-
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
@@ -50,6 +42,14 @@ if ingredients_list:
         use_container_width=True
        )
       
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
     #st.write(ingredients_string)
 
